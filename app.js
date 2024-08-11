12c009183e61c600dede19007bdcf282dd968411
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-08-11 17:02:24 -0500

    Eventlist array added

---
+++ app.js
@@ -0,0 +1,9 @@
+// Array list containing the event items
+const eventList = [{id:'Exam',suggests:['Schedule','Braindumps']}, 
+                   {id:'Wedding',suggests:['Print Cards','Reserve a Hotel']},
+                   {id:'Birthday Party',suggests:['Guest List','Cards']},
+                   {id:'Foreign Trip',suggests:['Passport','Currency']} ,
+                   {id:'Medical Checkup',suggests:['Insurance','Fasting']} 
+               ];
+
+               
